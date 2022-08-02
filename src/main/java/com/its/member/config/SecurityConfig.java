a6a3c6d91e4dfa38ee428971d4a189c7aca99989
AhnSangjun <sdjrdj@naver.com>
2022-08-02 17:02:55 +0900

    회원제게시판 정보수정과 로그인페이지등 추가중

---
+++ src/main/java/com/its/member/config/SecurityConfig.java
@@ -0,0 +1,15 @@
+package com.its.member.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+    }
+}
